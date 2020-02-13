1e106c00cc9a9fa1ec70961ebde33196ea165ffc
sabrinasioli <sabrina.sioly@gmail.com>
2020-02-13 10:08:10 -0300

    3 arq

---
+++ if.py
@@ -0,0 +1,16 @@
+numero = int(input('Digite um numero:' ))
+
+print('O numero digitado foi:', numero)
+
+if numero == 42:
+	print(';D')
+elif numero < 0:
+	print('numero negativo.')
+elif numero == 0:
+	print('numero nulo.')
+else:
+	print('numero positivo.')
+
+print('Obrigado por digitar.')
+
+
---
+++ while.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um número: '))
+cont = 1
+
+while(cont < 6):
+	print(num+cont)
+	cont += 1 
+print('final')
